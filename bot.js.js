f28242d8fdbdf57dd5342c378c8367a00a62e016
LILRXQZ <46800306+LILRXQZ@users.noreply.github.com>
2019-01-31 20:17:52 +0300

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -498,4 +498,4 @@ client.on("guildCreate", guild => {
   client.channels.get("476342429575020544").send(embed)
 });
 
-client.login('NTEzMDE5NTAxOTAyNTYxMjkw.DtG1eA.M2d1Eam2kT_ClAP5XyI8xxHTens');
+client.login('NTQwNTc0MjAyMjI3NTg5MTUx.DzS4-A.S3-X1IaDhr3ppMHkySdZHHXvovU');
